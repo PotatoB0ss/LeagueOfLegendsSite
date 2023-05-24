3ff87ee75db22034fb51591d41003b92f21238c7
PotatoB0ss <104912048+PotatoB0ss@users.noreply.github.com>
2023-05-24 12:38:18 +0300

    Delete target directory

--- target/classes/static/js/captchaFill.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('form').submit(function(event) {
-        var recaptchaResponse = grecaptcha.getResponse();
-        $('#recaptchaResponse').val(recaptchaResponse);
-    });
-});
